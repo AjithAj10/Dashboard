0fa3e938772eb7332bcefaf98339249c49120fd3
AjithAj10 <104498680+AjithAj10@users.noreply.github.com>
2023-06-05 11:03:17 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../styles.css";
 import TopBar from "./TopBar";
 import Overview from "./Overview";
 import GraphSection from "./Graphs/GraphSection";
 import Table from "./Table/Table";
 const Dashboard = () => {
-  const [data, setData] = useState(null);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch("http://localhost:9000/employe/");
-      let fetchedData = await response.json();
-
-      const filteredData = fetchedData.slice(20, 60);
-      setData(filteredData);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const chartOptions = {
-    responsive: true,
-  };
+ 
 
   return (
     <div className="chart">
